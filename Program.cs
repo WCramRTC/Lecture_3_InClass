4c8d572a5ccd18246184303d315c105b666bd9ae
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-09-26 20:05:19 -0700

    Updated Project

--- Program.cs
+++ Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // string Iphone Name
+            // string Iphone version number
+            // int Iphone capacity
+            // string color
+
+            // name
+            // version number
+            // capacity
+            // color
+            string[] phoneNames = { "Samsung Ultra", "IPhone", "Google Pixel", "IPhone" , "Google Pixel"};
+            string[] versionNumbers = { "22", "15", "7", "14", "2"};
+            int[] sizes = { 128, 256, 512, 2, 8};
+            string[] colors = { "Blue", "Green", "Purple" , "Pink", "Pink"};
+
+            Console.WriteLine("What color would you like to see?");
+            string search = Console.ReadLine();
+
+
+            // How do i only print the pink ones
+            // If the color is Pink, print the phone
+            for(int i = 0; i < phoneNames.Length; i++)
+            {
+
+                if (colors[i] == search)
+                {
+                    string phoneName = phoneNames[i];
+                    string VersionNumber = versionNumbers[i];
+                    int size = sizes[i];
+                    string color = colors[i];
+
+                    string formattedString = $"{phoneName} {VersionNumber} {size}Gb - {color}";
+
+                    Console.WriteLine(formattedString);
+                }
+    
+
+            }
+
+
+    
+
         }
     }
 }
